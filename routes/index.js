45357100b5cb4f8e6928ab286767d3584bc907ff
MarianaSanchezGonzalez03 <mari98san33@gmail.com>
2023-08-02 03:00:13 -0600

    Obteniendo ordenes del perfil1

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ const orderRouter = require('./orders.router');
 const customersRouter = require('./customers.router');
 const authRouter = require('./auth.router');
 
+const profileRouter = require('./profile.router');
+
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
@@ -16,6 +18,7 @@ function routerApi(app) {
   router.use('/orders', orderRouter);
   router.use('/customers', customersRouter);
   router.use('/auth', authRouter);
+  router.use('/profile', profileRouter);
 }
 
 module.exports = routerApi;
--- services/order.service.js
+++ services/order.service.js
@@ -16,6 +16,20 @@ class OrderService {
     const newItem = await models.OrderProduct.create(data);
     return newItem;
   }
+  async findByUser(userId) {
+    const orders = await models.Order.findAll({
+      where: {
+        '$customer.user.id$': userId
+      },
+      include: [
+        {
+          association: 'customer',
+          include: ['user']
+        }
+      ]
+    });
+    return orders;
+  }
 
   async find() {
     return [];
